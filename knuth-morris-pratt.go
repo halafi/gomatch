1199d907803f5a9eb00780a3055ca8d812f0a577
halafi <xgam33@gmail.com>
2013-10-22 15:01:26 +0200

    commit test

---
+++ knuth-morris-pratt.go
@@ -0,0 +1,14 @@
+﻿package main //package main required for standalone executable
+import "fmt" //implements fomratted I/O.
+import "os" //used for accessing command-line arguments
+
+/* implementation of Knuth-Morris-Pratt alghoritm
+   @author = Filip Halas
+*/
+
+func main() {
+
+    fmt.Println(os.Args);
+}
+
+
