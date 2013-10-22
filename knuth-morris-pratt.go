404848853377a47b1ba89d1b9bfaf6b6991bb3a6
halafi <xgam33@gmail.com>
2013-10-22 15:36:01 +0200

    command line reading

--- knuth-morris-pratt.go
+++ knuth-morris-pratt.go
@@ -1,14 +1,24 @@
 ﻿package main //package main required for standalone executable
 import "fmt" //implements fomratted I/O.
-import "os" //used for accessing command-line arguments
+import "os" //accessing command-line arguments
+import "log"
 
-/* implementation of Knuth-Morris-Pratt alghoritm
-   @author = Filip Halas
+/* 	implementation of Knuth-Morris-Pratt alghoritm.
+	Requires two command line arguments.
+	
+	@param string to be searched in (text)
+	@param string to be searched for (pattern)
 */
 
 func main() {
+	args := os.Args;
+	err := len(args);
+	if (err > 3) {
+		log.Fatal(err, " Too many arguments. Two separated by spaces are required. ");
+	}
 
-    fmt.Println(os.Args);
+	fmt.Printf("Text: \n %s\n", args[1]);
+	fmt.Printf("Pattern: \n %s\n", args[2]);
 }
 
 
