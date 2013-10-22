f8f693e3b663060085f3789c2159e20d3b4aebc6
halafi <xgam33@gmail.com>
2013-10-22 18:23:32 +0200

    knuth-morris-pratt.go error handling

--- knuth-morris-pratt.go
+++ knuth-morris-pratt.go
@@ -1,24 +1,31 @@
 ﻿package main //package main required for standalone executable
 import "fmt" //implements fomratted I/O.
 import "os" //accessing command-line arguments
-import "log"
+import "log" //simple logging package
 
 /* 	implementation of Knuth-Morris-Pratt alghoritm.
 	Requires two command line arguments.
 	
-	@param string to be searched in (text)
-	@param string to be searched for (pattern)
+	@argument string to be searched "in" (text)
+	@argument string to be searched "for" (pattern)
 */
 
 func main() {
+	// Error handling & declaration
 	args := os.Args;
-	err := len(args);
-	if (err > 3) {
-		log.Fatal(err, " Too many arguments. Two separated by spaces are required. ");
+	if (len(args) > 3) {
+		log.Fatal("Too many arguments. Two string arguments separated by spaces are required.");
+	} 
+	if (len(args) <= 2) {
+		log.Fatal("Not enough arguments. Two string arguments separated by spaces are required.");
 	}
-
-	fmt.Printf("Text: \n %s\n", args[1]);
-	fmt.Printf("Pattern: \n %s\n", args[2]);
+	text := args[1];
+	pattern := args[2];
+	if (len(args[1])<len(args[2])) {
+		log.Fatal("Pattern is longer than text.");
+	}
+	// Alghoritm execution
+	fmt.Printf("Running: Knuth-Morris-Pratt alghoritm.\nText: %s\nPattern: %s\n", text, pattern);
 }
 
 
