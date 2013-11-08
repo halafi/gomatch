13c191141a1d13b9dd54ee6aaa667a4589bb1600
halafi <xgam33@gmail.com>
2013-11-08 18:31:19 +0100

    bom - working oracle construction

--- string matching/bom-file.go
+++ string matching/bom-file.go
@@ -45,21 +45,21 @@ func bom(t, p string) {
 	return
 }
 
-/*	Construction of the factor oracle automaton
+/*	Construction of the factor oracle automaton for a word p.
 
 	@param p pattern to be added
 	@param supply supply map
 	@return oracle built oracle
 */
 func oracleOnLine(p string)(oracle map[int]map[uint8]int) {
-	//create Oracle(e) with one single initial state 0 & S(0) = empty
+	//m := len(p)
 	oracle = make(map[int]map[uint8]int)
-	supply := make(map[int]int) //supply function
+	supply := make([]int, len(p)+2) //supply function
 	createNewState(0, oracle)
-	supply[0]=0
-	//add the whole pattern p
+	supply[0]=-1
+	var orP string
 	for j := 0; j < len(p); j++ {
-		oracleAddLetter(oracle, supply, p, p[j])
+		oracle, orP = oracleAddLetter(oracle, supply, orP, p[j])
 	}
 	return oracle
 }
@@ -67,31 +67,30 @@ func oracleOnLine(p string)(oracle map[int]map[uint8]int) {
 /*	Adds one letter to the oracle.
 
 	@param oracle oracle to add letter to
-	@param p pattern
+	@param p pattern (not whole, contained in oracle)
 	@param o letter to be added
 	@param supply supply map
 */
-func oracleAddLetter(oracle map[int]map[uint8]int, supply map[int]int, p string, o uint8) { 
-	m := len(p)
+func oracleAddLetter(oracle map[int]map[uint8]int, supply []int, orP string, o uint8)(oracleToReturn map[int]map[uint8]int, orPToReturn string) { 
+	m := len(orP)
 	var s int
 	createNewState(m + 1, oracle)
-	createNewState(m, oracle)
-	delta(m, o, m + 1, oracle) //delta(m,o) = m + 1
+	//createNewState(m, oracle)
+	createTransition(m, o, m + 1, oracle)
 	k := supply[m] //0 (nil) the first time
-	fmt.Printf("For char %c, %t, %t.\n",o, k != 0, getDelta(k,o, oracle) == 0)
-	for k != 0 && getDelta(k,o, oracle) == 0 {
-		fmt.Printf("We are in this for cycle, yo! \n")
-		delta(k, o, m + 1, oracle)
+	for k > -1 && getTransition(k,o, oracle) == 0 {
+		createTransition(k, o, m + 1, oracle)
+		k = supply[k]
 	}
-	if (k == 0) {
+	if (k == -1) {
 		s = 0
 	} else {
-		s = getDelta(k,o, oracle)
+		s = getTransition(k,o, oracle)
 	}
-	fmt.Printf("%d", s)
 	supply[m+1] = s
-	//k je vždycky 0
+	return oracle, orP+string(o)
 }
+
 
 // 	Follows some automaton functions.
 //	Automaton states are stored in map[int]map[uint8]int:
@@ -99,7 +98,6 @@ func oracleAddLetter(oracle map[int]map[uint8]int, supply map[int]int, p string,
 //		- lets assume, that state 0 is always the inital state of the automaton
 
 /*	Automaton function for adding a new state.
-	
 	@param state state number to add state for
 	@param oracle oracle to add state to
 	@return oracle oracle with added state
@@ -107,24 +105,24 @@ func oracleAddLetter(oracle map[int]map[uint8]int, supply map[int]int, p string,
 func createNewState(state int, oracle map[int]map[uint8]int) {
 	emptyMap := make(map[uint8]int)
 	oracle[state] = emptyMap
+	fmt.Printf("\n State %d was created", state)
 }
 
-//	Supply function S for external transitions. Supply state j for each state i (j<i), denoted j=S(i). Build together, S(0) = 0.
-
 /* 	Automaton function for adding a transition from 'state' over 'letter' to 'end' state.
-	@usage delta(fromSate, overChar, toState, oracle)
+	@usage createTransition(fromSate, overChar, toState, oracle)
 	@return oracle oracle with added transition
 */
-func delta(fromState int, overChar uint8, toState int, oracle map[int]map[uint8]int) {
+func createTransition(fromState int, overChar uint8, toState int, oracle map[int]map[uint8]int) {
 	stateMap := oracle[fromState]
 	stateMap[overChar]= toState
 	oracle[fromState] = stateMap
+	fmt.Printf("\n σ(%d,%c)=%d;",fromState,overChar,toState)
 }
 
 /*	Returns a 'toState' from state 'fromState' over char 'overChar' :).
-	@return toState state for the desired delta function
+	@return toState state for the desired transition
 */
-func getDelta(fromState int, overChar uint8, oracle map[int]map[uint8]int)(toState int) {
+func getTransition(fromState int, overChar uint8, oracle map[int]map[uint8]int)(toState int) {
 	stateMap := oracle[fromState]
 	toState = stateMap[overChar]
 	return toState
