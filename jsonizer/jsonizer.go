5ec631b57f3c5b5f666ba7301a30d340067f29bb
halafi <xgam33@gmail.com>
2013-11-20 20:41:32 +0100

    work on jsonizer.go

--- jsonizer/jsonizer.go
+++ jsonizer/jsonizer.go
@@ -1,5 +1,5 @@
 package main
-import ("fmt"; "log"; "strings"; "io/ioutil"; "time")
+import ("fmt"; "log"; "strings"; "io/ioutil"; "time"; /*"regexp"*/)
 
 /** 
 	User defined.
@@ -7,9 +7,9 @@ import ("fmt"; "log"; "strings"; "io/ioutil"; "time")
 	@true prints various extra stuff out, but slows down the execution
 	@false will be quick and quiet
 */
-const debugMode bool = false
+const debugMode bool = true
 
-/**
+/** udelat si pole array of matches a do nej ukladat pro kazdy radek match a nakonec vypsat
  	THE BIG ONE.
 */
 func main() {
@@ -21,69 +21,80 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	matchLines := strings.Split(string(patFile), "\n")
-	regexesPerLine, wordsPerLine, p := separate(matchLines) //declaration
-	fmt.Printf("\nJSONIZER v.00000000002\n----------------------\n----------------------\n")
+	matches := strings.Split(string(patFile), "\n") // match number + what needs to match
+	lineMatch := make(map[int]string) // line number + matches we found with repetitions i.g.: lineMathc[0] = "IP IP word word", which will get compared against all the matches
+	lineMatch = lineMatch
+	r, p := process(matches) //we get all regexes and all patterns
+	fmt.Printf("\nJSONIZER\n----------------------\n----------------------\n")
 	if debugMode==true { 
-		fmt.Printf("\nThere are %d matches defined in 'patterns.txt':\n",len(matchLines))
-		for i := range matchLines {
-			fmt.Printf("Match %d: %s\n", i, matchLines[i])
-			fmt.Printf("Regex:")
-			for j:= range regexesPerLine[i] {
-				fmt.Printf("%s", regexesPerLine[i][j])
-			}
-			fmt.Printf(", Words:")
-			for j:= range wordsPerLine[i] {
-				fmt.Printf("%s", wordsPerLine[i][j])
-			}
-			fmt.Println()
+		fmt.Printf("\nThere are %d matches defined in 'patterns.txt':\n",len(matches))
+		for i := range matches {
+			fmt.Printf("Match %d: %s\n", i, matches[i])
+		}
+		fmt.Printf("\nAll regular expressions: ")
+		for j:= range r {
+			fmt.Printf("%s ", r[j])
+		}
+		fmt.Printf("\nAll words to search for: ")
+		for j:= range r {
+			fmt.Printf("%s ", p[j])
 		}
 		fmt.Printf("\nI shall search for them in log file 'text.txt' that is %d chars long!\n\n",len(textFile))
 	}
+	
 	t := string(textFile)
 	startTime := time.Now()
 	//
 	// SEARCHING
 	//
-	occurences := make(map[int][]int)
 	ac, f, s := buildAc(p)
 	if debugMode==true {
 		fmt.Printf("\n\nLETS DO IT:\n\n")
 	}
 	textLines := strings.Split(t, "\n")
 
-	for line := range textLines { //prochazeni po radcich
+	for line := range textLines { 
+	//prochazeni po radcich	
 		t = textLines[line]
-		current := 0
-		for pos := 0; pos < len(t); pos++ {
-			if debugMode==true {
-				fmt.Printf("Position: %d, we read: %c", pos, t[pos])
-			}
-			for getTransition(current, t[pos], ac) == -1 && s[current] != -1 {
-				current = s[current]
-			}
-			if getTransition(current, t[pos], ac) != -1 {
-				current = getTransition(current, t[pos], ac)
-				if debugMode==true {
-					fmt.Printf(" (Continue) \n")
+		
+		currentLine := strings.Split(textLines[line], " ") //rozdeleni radku na slova
+		for j:= range currentLine { //po slovech na soucasnem radku
+			current := 0 //state of automaton
+			for pos := 0; pos < len(currentLine[j]); pos++ {
+				//case1: hledame slova 
+				//fmt.Printf("Position: %d, we read: %c", pos, currentLine[j][pos])
+				for getTransition(current, currentLine[j][pos], ac) == -1 && s[current] != -1 {
+					current = s[current]
 				}
-			} else {
-				current = 0
-				if debugMode==true {
-					fmt.Printf(" (FAIL) \n")
+				if getTransition(current,currentLine[j][pos], ac) != -1 {
+					current = getTransition(current, currentLine[j][pos], ac)
+					//fmt.Printf(" (Continue) \n")
+
+				} else {
+					current = 0
+					//fmt.Printf(" (FAIL) \n")
 				}
-			}
-			_, ok := f[current]
-			if ok {
-				for i := range f[current] {
-					if p[f[current][i]] == getWord(pos-len(p[f[current][i]])+1, pos, t) { //check for word match
-						if debugMode==true {
-							fmt.Printf("Occurence at position %d, %q = %q\n", pos-len(p[f[current][i]])+1, p[f[current][i]], p[f[current][i]])
+				_, ok := f[current]
+				if ok { //dosli jsme do finalniho stavu pro nejaka slova
+					for i := range f[current] { //slova pro tento radek nalezena
+						if p[f[current][i]] == getWord(pos-len(p[f[current][i]])+1, pos, currentLine[j]) { //check for word match
+							/*if debugMode==true {
+								fmt.Printf("Occurence at line %d, %s = %s\n", line, p[f[current][i]], p[f[current][i]])
+							}*/
+							lineMatch[line] = lineMatch[line] + " " + p[f[current][i]]
 						}
-						newOccurences := intArrayCapUp(occurences[f[current][i]])
-						occurences[f[current][i]] = newOccurences
-						occurences[f[current][i]][len(newOccurences)-1] = pos-len(p[f[current][i]])+1
 					}
+				}
+			}
+			//case2: hledame regexy na tomto radku (vsechny mozne), pro kazde slovo zavolat match ...
+			for k := range currentLine[j] {
+				k=k
+				for l := range r {
+					if r[l] == currentLine[j] { //matched,err := regexp.MatchString((string(patFile)), "50")
+						//
+						fmt.Printf("pmatch")
+					}
+					
 				}
 			}
 		}
@@ -92,16 +103,9 @@ func main() {
 	//old output
 	elapsed := time.Since(startTime)
 	fmt.Printf("\n\nElapsed %f secs\n", elapsed.Seconds())
-	for key, value := range occurences { //prints all occurences of each pattern (if there was at least one)
-		fmt.Printf("\nThere were %d occurences for word: %q at positions: ",len(value), p[key])
-		for i := range value {
-			fmt.Printf("%d", value[i])
-			if i != len(value) - 1 {
-				fmt.Printf(", ")
-			}
-		}
-		fmt.Printf(".")
-	}
+	/*for i:=0; i<len(lineMatch); i++ {
+		fmt.Printf("\n Line %d: %s", i, lineMatch[i])
+	}*/
 	return
 }
 
@@ -109,29 +113,21 @@ func main() {
 	Takes lines of pattern's definitions and separates them into array's of words and regular expressions.
 	Also return's all words given in 'lines'.
 */
-func separate(lines []string)(regexesPerLine map[int][]string, wordsPerLine map[int][]string, allWords []string) {
-	regexesPerLine = make(map[int][]string)
-	wordsPerLine = make(map[int][]string)
+func process(lines []string)(allRegexes []string, allWords []string) {
+	allRegexes = make([]string, 0)
 	allWords = make([]string, 0)
 	for i := range lines {
 		line := strings.Split(lines[i], " ")
 		for j := range line {
 			if line[j][0] == '<' {
-				currentRegexes := regexesPerLine[i]
-				currentRegexes = stringArrayCapUp(currentRegexes)
-				currentRegexes[len(currentRegexes)-1] = line[j]
-				regexesPerLine[i] = currentRegexes
+				allRegexes = addWord(allRegexes, getWord(0, len(line[j])-1, line[j]))
 			}
 			if line[j][0] == '{' {
-				currentWords := wordsPerLine[i]
-				currentWords = stringArrayCapUp(currentWords)
-				currentWords[len(currentWords)-1] = getWord(1, len(line[j])-3, line[j])
-				allWords = addWord(allWords, currentWords[len(currentWords)-1])
-				wordsPerLine[i] = currentWords
+				allWords = addWord(allWords, getWord(1, len(line[j])-3, line[j]))
 			}
 		}
 	}
-	return regexesPerLine, wordsPerLine, allWords
+	return allRegexes, allWords
 }
 
 /**
@@ -143,9 +139,7 @@ func buildAc(p []string) (acToReturn map[int]map[uint8]int, f map[int][]int, s [
 	i := 0 //root of acTrie
 	acToReturn = acTrie
 	s[i] = -1
-	if debugMode==true {
-		fmt.Printf("\n\nAC construction: \n")
-	}
+
 	for current := 1; current < len(stateIsTerminal); current++ {
 		o, parent := getParent(current, acTrie)
 		down := s[parent]
@@ -157,28 +151,9 @@ func buildAc(p []string) (acToReturn map[int]map[uint8]int, f map[int][]int, s [
 			if stateIsTerminal[s[current]] == true {
 				stateIsTerminal[current] = true
 				f[current] = arrayUnion(f[current], f[s[current]]) //F(Current) <- F(Current) union F(S(Current))
-				if debugMode==true {
-					fmt.Printf(" f[%d] set to: ", current)
-					for i := range f[current] {
-						fmt.Printf("%d\n", f[current][i])
-					}
-				}
 			}
 		} else {
 			s[current] = i //initial state?
-		}
-	}
-	if debugMode==true {
-		fmt.Printf("\nsupply function: \n")
-		for i:= range s {
-			fmt.Printf("\ns[%d]=%d", i, s[i])
-		}
-		fmt.Printf("\n\n")
-		for i,j := range f {
-			fmt.Printf("f[%d]=", i)
-			for k := range j {
-				fmt.Printf("%d\n", j[k])
-			}
 		}
 	}
 	return acToReturn, f, s
@@ -196,9 +171,6 @@ func constructTrie (p []string) (trie map[int]map[uint8]int, stateIsTerminal []b
 	stateIsTerminal = make([]bool, 1)
 	f = make(map[int][]int) 
 	state := 1
-	if debugMode==true {
-		fmt.Printf("\n\nTrie construction: \n")
-	}
 	createNewState(0, trie)
 	for i:=0; i<len(p); i++ {
 		current := 0
@@ -219,16 +191,10 @@ func constructTrie (p []string) (trie map[int]map[uint8]int, stateIsTerminal []b
 		if stateIsTerminal[current] {
 			newArray := intArrayCapUp(f[current])
 			newArray[len(newArray)-1] = i
-			f[current] = newArray //F(Current) <- F(Current) union {i}
-			if debugMode==true {
-				fmt.Printf(" and %d", i)
-			}
+			f[current] = newArray
 		} else {
 			stateIsTerminal[current] = true
-			f[current] = []int {i}  //F(Current) <- {i}
-			if debugMode==true {
-				fmt.Printf("\n%d is terminal for word number %d", current, i) 
-			}
+			f[current] = []int {i}
 		}
 	}
 	return trie, stateIsTerminal, f
@@ -346,9 +312,6 @@ func getParent(state int, at map[int]map[uint8]int) (uint8, int) {
 */
 func createNewState(state int, at map[int]map[uint8]int) {
 	at[state] = make(map[uint8]int)
-	if debugMode==true {
-		fmt.Printf("\ncreated state %d", state)
-	}
 }
 
 /**
@@ -357,9 +320,6 @@ func createNewState(state int, at map[int]map[uint8]int) {
 */
 func createTransition(fromState int, overChar uint8, toState int, at map[int]map[uint8]int) {
 	at[fromState][overChar]= toState
-	if debugMode==true {
-		fmt.Printf("\n    σ(%d,%c)=%d;",fromState,overChar,toState)
-	}
 }
 
 /**
