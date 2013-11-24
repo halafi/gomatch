9b056088944db9206d810ec2649903f1ff016d17
halafi <xgam33@gmail.com>
2013-11-24 15:52:49 +0100

    regex tester improved

---
+++ jsonizer/regex tester/readme.txt
@@ -0,0 +1,6 @@
+Simple tester of regular expression in Go.
+
+Define regex + word to match in testedRegex.txt on a single line like this:
+^[0-9]+$ das0
+
+Prints true if das0 matches as number, false otherwise.
--- jsonizer/regex tester/test.go
+++ jsonizer/regex tester/test.go
@@ -1,12 +1,14 @@
 ﻿package main
-import ("regexp"; "fmt";"io/ioutil";)
+import ("regexp"; "fmt";"io/ioutil"; "strings")
 
 func main() {
 	/*The syntax of the regular expressions accepted is the same general syntax used by Perl,
 	PYTHON!, and other languages. More precisely, it is the syntax accepted by RE2 and described
 	at http://code.google.com/p/re2/wiki/Syntax, except for \C.*/
 	patFile,_ := ioutil.ReadFile("testedRegex.txt")
-	matched,err := regexp.MatchString((string(patFile)), "1906")
-	fmt.Println(matched, err)
+	patText := strings.Split(string(patFile), " ")
+	matched,err := regexp.MatchString(patText[0], patText[1])
+	fmt.Printf("\n%t\n\n\nErrors: ", matched)
+	fmt.Println(err)
 }
 	
